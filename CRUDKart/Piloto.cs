13eb88d2172e69200a9159a10ba2d650a261150d
luisp <luisp@G15_FELIPE>
2025-07-22 15:42:30 -0300

    Adicionar arquivos de projeto.

---
+++ CRUDKart/Piloto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDKart
+{
+    public class Piloto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public int Cpf { get; set; }
+    }
+}
---
+++ CRUDKart/PilotoService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDKart
+{
+    public class PilotoService
+    {
+        private readonly List<Piloto> _pilotos = new();
+        private int _proximoID = 1;
+
+        public void Cadastrar()
+        {
+            Console.WriteLine("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("CPF: ");
+            int cpf = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            _pilotos.Add(new Piloto
+            {
+                Id = _proximoID++,
+                Nome = nome,
+                Cpf = cpf,
+                Idade = idade
+            });
+            
+            Console.WriteLine("Piloto cadastrado com sucesso!");
+        }
+    }
+}
